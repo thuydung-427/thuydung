7454cebc41ab2e0f7466773895582b21136a2630
thuydung-427 <daothithuydung04022007@gmail.com>
2025-02-28 14:11:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function login()
   let u = username.value
   let p = pw.value
   if (u== "thuydung"&& p= "123")
-    }
+    {
       window.open("home.html")
       document.getElementById(compiler).style.display="block"
     }
