a3063c4691df44010273a4cc0515befeddcbe528
thuydung-427 <daothithuydung04022007@gmail.com>
2025-02-28 14:02:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function login()
 {
   let u = username.value
   let p = pw.value
-  if (u=="hieu"&& p="123")
+  if (u== "hieu"&& p= "123")
     }
-window.open("home.html")
-document.getElementById(compiler).style.display="block"
+      window.open("home.html")
+      document.getElementById(compiler).style.display="block"
     }
 }
